c0842ef4ae3f149b259bf65d82b20868e9618d26
Udaybari-Dev <udaybari17@gmail.com>
2025-09-18 11:40:55 +0530

    resolve routing and import issues in React frontend

--- frontend/service/apiClient.js
+++ frontend/service/apiClient.js
@@ -10,6 +10,29 @@ class ApiClient{
         };
      }
 
+     async customFetch(ennpoint , option = {}) {
+        try{
+
+            const url = `${this.baseUrl}${endpoint}`;
+            const headers = {...this.defaultHeaders , ...options.headers}
+
+            const config = {
+                ...options ,
+                headers,
+                Credentials : 'include'
+            }
+
+            console.log(`Fetching : ${url}`);
+            
+             
+
+        }catch(error){
+            console.error("Api call error " , error);
+            throw error
+
+        }
+     }
+
 
 
 
